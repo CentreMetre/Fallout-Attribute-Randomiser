2d5ae8c1461dd837e3d14d9e84899b1c96539c3d
Martin McLaren <luckyminer01@gmail.com>
2023-05-29 18:34:30 +0100

    0.1.0.2.1
    Switch to netbeans
    
    Manually switched to NetBeans by manually moving files

---
+++ src/falloutattributerandomiser/FalloutAttributeRandomiser.java
@@ -0,0 +1,18 @@
+package falloutattributerandomiser;
+
+/**
+ *
+ * @author Martin McLaren
+ */
+public class FalloutAttributeRandomiser
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/falloutattributerandomiser/Player.java
@@ -0,0 +1,9 @@
+package falloutattributerandomiser;
+
+/**
+ * @since 0.1.0.
+ * @author Martin McLaren
+ */
+public class Player {
+
+}
